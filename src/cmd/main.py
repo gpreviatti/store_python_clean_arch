e932472f50f719fe3891e19775b65eee42da70cc
Giovanni Brunno Previatti <gpreviatt@gmail.com>
2023-02-09 19:51:15 -0300

    first commit

---
+++ src/cmd/main.py
@@ -0,0 +1,5 @@
+from usecases.calculations_usecase import CalculationsUseCase
+
+calculation = CalculationsUseCase(1, 2)
+
+print(calculation.get_product())
---
+++ tests/test_my_calculations.py
@@ -0,0 +1,19 @@
+import unittest
+from src.usecases.calculations_usecase import CalculationsUseCase
+
+class TestCalculations(unittest.TestCase):
+
+    def setUp(self):
+        self.calculation = CalculationsUseCase(8, 2)
+
+    def test_sum(self):
+        self.assertEqual(self.calculation.get_sum(), 10, 'The sum is wrong.')
+
+    def test_diff(self):
+        self.assertEqual(self.calculation.get_difference(), 6, 'The difference is wrong.')
+
+    def test_product(self):
+        self.assertEqual(self.calculation.get_product(), 16, 'The product is wrong.')
+
+    def test_quotient(self):
+        self.assertEqual(self.calculation.get_quotient(), 4, 'The quotient is wrong.')
